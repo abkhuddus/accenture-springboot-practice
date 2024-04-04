3f1b22c53106d124a5fda0403ab73166be4b7b60
abkhuddus <abjava121@gmail.com>
2024-04-04 17:28:59 +0530

    spring boot project starting building mongo project

---
+++ src/main/java/com/mongo/Mycontroller.java
@@ -0,0 +1,20 @@
+package com.mongo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Mycontroller {
+
+
+
+    @GetMapping("/load")
+    public ResponseEntity<?> loadMassage( ){
+    return  new ResponseEntity<String>("Hi ", HttpStatus.ACCEPTED);
+    }
+}
+
+
+
