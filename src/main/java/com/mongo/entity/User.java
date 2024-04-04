6911036e35c7f867e520ce40540ff73cee2cc27a
abkhuddus <abjava121@gmail.com>
2024-04-04 20:11:56 +0530

    User entity created

---
+++ src/main/java/com/mongo/entity/User.java
@@ -0,0 +1,14 @@
+package com.mongo.entity;
+
+public class User {
+
+    private Integer id;
+
+    private String name;
+
+    private String email;
+
+    private String address;
+
+    private String mobileNumber;
+}
