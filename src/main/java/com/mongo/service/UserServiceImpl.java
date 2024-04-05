f33e95dddb63b1bf17efd7d83a1e44b39092b709
abkhuddus <abjava121@gmail.com>
2024-04-05 10:54:29 +0530

    Service Layer added for User module

---
+++ src/main/java/com/mongo/service/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.mongo.service;
+
+public class UserServiceImpl implements UserService{
+    @Override
+    public String loadUser() {
+        return " UserName: abkhuddus , pwd : Abkhuddus@Uzma4";
+    }
+}
